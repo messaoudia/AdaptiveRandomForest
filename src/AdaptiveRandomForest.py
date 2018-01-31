13f927f8e73a2290939ea1815774ab9af927e8c1
Amin Messaoudi <amin_messaoudi@yahoo.com>
2018-01-31 17:51:02 +0100

    Updating the constructor

--- src/AdaptiveRandomForest.py
+++ src/AdaptiveRandomForest.py
@@ -1,3 +1,6 @@
 class AdaptiveRandomForest:
-   def __init__(self):
-       print("Let's go")
+    def __init__(self, m, n, delta_w=0.0001, delta_d=0.00001):
+        self.m = m
+        self.n = n
+        self.delta_w = delta_w
+        self.delta_d = delta_d
