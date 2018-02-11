Fixed errors. TO DO Average voting

src/ARFHoeffdingTree.py
@@ -20,7 +20,7 @@ class ARFHoeffdingTree (HoeffdingTree):
     def fit(self, X, y, classes=None, weight=None):
         pass
 
-    def __init__(self, m, delta_w=0.0001, delta_d=0.00001,):
+    def __init__(self, m, delta_w=0.01, delta_d=0.001,):
         super().__init__()
         self.m = m
         self.remove_poor_atts = None
@@ -29,7 +29,8 @@ class ARFHoeffdingTree (HoeffdingTree):
         self.delta_drift = delta_d
         self.adwin_warning = ADWIN(delta=self.delta_warning)
         self.adwin_drift = ADWIN(delta=self.delta_drift)
-
+        self.leaf_prediction = 'nb'
+        self.grace_period = 50
 
     @staticmethod
     def is_randomizable():
@@ -72,6 +73,8 @@ class ARFHoeffdingTree (HoeffdingTree):
             ht: The Hoeffding Tree
 
             """
+            if self.num_attributes <= 0 or self.num_attributes > len(X) - 1:
+                self.num_attributes = len(X) - 1
             if not self._is_initialized:
                 self._attribute_observers = [None] * self.num_attributes
                 self._is_initialized = True
@@ -103,10 +106,13 @@ class ARFHoeffdingTree (HoeffdingTree):
             super().__init__(initial_class_observations, m)
 
         def get_class_votes(self, X, ht):
+            observed_x = []
+            for attr_index in self.list_attributes:
+                observed_x.append(X[attr_index])
             if self.get_weight_seen() >= ht.nb_threshold:
-                return do_naive_bayes_prediction(X, self._observed_class_distribution, self._attribute_observers)
+                return do_naive_bayes_prediction(np.asarray(observed_x), self._observed_class_distribution, self._attribute_observers)
             else:
-                return super().get_class_votes(X, ht)
+                return super().get_class_votes(np.asarray(observed_x), ht)
 
         def disable_attribute(self, att_index):
             # Should not disable poor attributes, they are used in NB calculation
@@ -133,6 +139,9 @@ class ARFHoeffdingTree (HoeffdingTree):
             super().learn_from_instance(X, y, weight, ht)
 
         def get_class_votes(self, X, ht):
+            observed_x = []
+            for attr_index in self.list_attributes:
+                observed_x.append(X[attr_index])
             if self._mc_correct_weight > self._nb_correct_weight:
                 return self._observed_class_distribution
-            return do_naive_bayes_prediction(X, self._observed_class_distribution, self._attribute_observers)
+            return do_naive_bayes_prediction(np.asarray(observed_x), self._observed_class_distribution, self._attribute_observers)

src/AdaptiveRandomForest.py
@@ -1,10 +1,11 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 from src.ARFHoeffdingTree import ARFHoeffdingTree
 import numpy as np
 
+
 class AdaptiveRandomForest:
 
-    def __init__(self, m, n, predict_method="avg"):
+    def __init__(self, m, n, predict_method="mc"):
         """
         Constructor
         :param m: maximum feature evaluated per split
@@ -75,18 +76,19 @@ class AdaptiveRandomForest:
                     if tree.adwin_drift.detected_change():
                         if self.B.get(key, None) is None:
                             # Added condition, there is some problem here, we detected a drift before warning
-                            b = self.create_tree()# Also too many trees is being created
+                            b = self.create_tree()  # Also too many trees is being created
                             self.B[key] = b
                         new_tree.append(self.B[key])
                         index_to_replace.append(key)
                 tree.rf_tree_train(np.asarray([X_]), np.asarray([y_]))
 
             for key, value in self.B.items():
-                value.rf_tree_train(np.asarray([X_]), np.asarray([y_])) # Changed
+                value.rf_tree_train(np.asarray([X_]), np.asarray([y_]))  # Changed
 
             # tree ← B(tree)
             for key, index in enumerate(index_to_replace):
-                self.Trees[key] = new_tree[key]
+                self.Trees[index] = new_tree[key]
+                self.B.pop(index)
 
             new_tree.clear()
             index_to_replace.clear()
@@ -94,14 +96,14 @@ class AdaptiveRandomForest:
     def predict(self, X):
         best_class = -1
         # average weight
-        if self.predict_method == "avg":
-            predictions = defaultdict(float)
-            predictions_count = defaultdict(int)
+        predictions = defaultdict(float)
+        predictions_count = defaultdict(int)
 
-            for index, val in enumerate(self.Trees.items()):
-                tree = self.Trees[index]
+        if self.predict_method == "avg":
+
+            for key, tree in self.Trees.items():
                 y_predicted = tree.predict(X)
-                predictions[y_predicted[0]] += self.Weights[index][0] / self.Weights[index][1]
+                predictions[y_predicted[0]] += self.Weights[key][0] / self.Weights[key][1]
                 predictions_count[y_predicted[0]] += 1
 
             max_weight = -1.0
@@ -113,7 +115,14 @@ class AdaptiveRandomForest:
 
         # TODO majority class
         elif self.predict_method == "mc":
-            return best_class
+            for key, tree in self.Trees.items():
+                y_predicted = tree.predict(X)
+                predictions_count[y_predicted[0]] += 1
+            max_value = -1.0
+            for key, value in predictions_count.items():
+                if value > max_value:
+                    best_class = key
+                    max_value = value
 
         p = list()
         p.append(best_class)